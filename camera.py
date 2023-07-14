6da60c9873307923e0dbc4c7fa743851ca83c9db
thanh <thanhdinhngoclap123@gmail.com>
2023-07-14 19:40:11 +0700

    Explanatory

--- camera.py
+++ camera.py
@@ -2,10 +2,14 @@ import streamlit as st
 from PIL import Image
 
 with st.expander("Tách tách mfcker"):
+    #mở cửa sổ chụp anh bằng method camera input
     camera_image = st.camera_input("Chụp ảnh không em")
 
 
 if camera_image:
+        # tạo instance của class Image
         img = Image.open(camera_image)
+        #convert gray scale
         gray_scale = img.convert("L")
+        #render ảnh gray scale ra màn hình
         st.image(gray_scale)
